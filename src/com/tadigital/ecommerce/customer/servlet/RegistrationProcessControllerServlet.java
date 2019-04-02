ea23fb7b94b5921cc6c935dbabe89987e61f2b69
Mahimitra <mahimitra.c@tadigital.com>
2019-04-02 18:43:55 +0530

    RegistrationProcessController file is added

---
+++ src/com/tadigital/ecommerce/customer/servlet/RegistrationProcessControllerServlet.java
@@ -0,0 +1,67 @@
+package com.tadigital.ecommerce.customer.servlet;
+
+import java.io.*;
+import java.util.*;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.tadigital.ecommerce.customer.entity.Customer;
+import com.tadigital.ecommerce.customer.service.CustomerService;
+
+@WebServlet("/register")
+public class RegistrationProcessControllerServlet extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		RequestDispatcher rd = req.getRequestDispatcher("RegistrationForm.html");
+		rd.forward(req, resp);
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		String name[]=req.getParameter("f1").split(" ");
+		String firstName = name[0];
+	
+		String lastName;
+		if(name.length==1) {lastName="null";}
+		else {lastName=name[1];}
+		//String dateOfJoining = req.getParameter("f3");
+		String email = req.getParameter("f2");
+		String password = req.getParameter("f3");
+		
+		Customer customer = new Customer();
+		customer.setFirstName(firstName);
+		customer.setLastName(lastName);
+		
+		/*String[] date = dateOfJoining.split("-");
+		int yyyy = Integer.parseInt(date[0]);
+		int mm = Integer.parseInt(date[1]);
+		
+		int dd = Integer.parseInt(date[2]);
+		employee.setDateOfJoining(new GregorianCalendar(yyyy, mm, dd));
+		
+		*/
+		customer.setEmail(email);
+		customer.setPassword(password);
+		
+		CustomerService customerService = new CustomerService();
+		boolean status = customerService.registerCustomer(customer);
+		if(status) {
+			HttpSession ses = req.getSession();
+			ses.setAttribute("RegStatus", "true");
+			RequestDispatcher rd = req.getRequestDispatcher("index.jsp");
+			rd.forward(req, resp);
+		} 
+		else {
+			HttpSession ses = req.getSession();
+			ses.setAttribute("RegStatus", "false");
+		RequestDispatcher rd = req.getRequestDispatcher("index.jsp");
+		rd.forward(req, resp);}
+	}
+}
