8c5fbd2324a7fdea1228878b1a76229345f50100
Mahimitra <37669380+Mahimitra@users.noreply.github.com>
2019-04-07 00:33:46 +0530

    Update LoginProcessControllerServlet.java
    Updated after sonar lint suggestions

--- src/com/tadigital/ecommerce/customer/servlet/LoginProcessControllerServlet.java
+++ src/com/tadigital/ecommerce/customer/servlet/LoginProcessControllerServlet.java
@@ -13,40 +13,59 @@ import javax.servlet.http.HttpSession;
 import com.tadigital.ecommerce.customer.entity.Customer;
 import com.tadigital.ecommerce.customer.service.CustomerService;
 
+@SuppressWarnings("serial")
 @WebServlet("/login")
 public class LoginProcessControllerServlet extends HttpServlet {
 	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
 		RequestDispatcher rd = req.getRequestDispatcher("LoginForm.html");
-		rd.forward(req, resp);
+		try {
+			rd.forward(req, resp);
+		} catch (ServletException | IOException e) {
+
+			e.printStackTrace();
+		}
 	}
-	
+
 	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) {
 		String email = req.getParameter("f1");
 		String password = req.getParameter("f2");
-		
-		
-		
+
 		Customer customer = new Customer();
 		customer.setEmail(email);
 		customer.setPassword(password);
-		
+
 		HttpSession ses1 = req.getSession();
 		ses1.setAttribute("Email", email);
 		ses1.setAttribute("pwd", password);
 		CustomerService customerService = new CustomerService();
-		boolean status = customerService.loginCustomer(customer);	
-		if(status) {
-			
-			
-			RequestDispatcher rd = req.getRequestDispatcher("customerinfo.html");
-			rd.forward(req, resp);
+		boolean status = customerService.loginCustomer(customer);
+		String name = null;
+
+		name = customer.getFirstName() + customer.getLastName();
+
+		ses1.setAttribute("name", name);
+
+		if (status) {
+
+			RequestDispatcher rd = req.getRequestDispatcher("customerinfo.jsp");
+			try {
+				rd.forward(req, resp);
+			} catch (ServletException | IOException e) {
+
+				e.printStackTrace();
+			}
 		} else {
 			HttpSession ses = req.getSession();
 			ses.setAttribute("LogStatus", "false");
-			RequestDispatcher rd = req.getRequestDispatcher("index.jsp");
-			rd.forward(req, resp);
+			RequestDispatcher rd = req.getRequestDispatcher("signinsignup.jsp");
+			try {
+				rd.forward(req, resp);
+			} catch (ServletException | IOException e) {
+
+				e.printStackTrace();
+			}
 		}
 	}
 }
