f5dbdd1adb806771372b90163caf1fee1bd6b67a
Mahimitra <37669380+Mahimitra@users.noreply.github.com>
2019-04-07 23:53:38 +0530

    Add files via upload
    Updated class after personaldetails controller

--- src/com/tadigital/ecommerce/customer/dao/CustomerDao.java
+++ src/com/tadigital/ecommerce/customer/dao/CustomerDao.java
@@ -1,4 +1,4 @@
-	package com.tadigital.ecommerce.customer.dao;
+package com.tadigital.ecommerce.customer.dao;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
@@ -11,26 +11,26 @@ import java.util.GregorianCalendar;
 import com.tadigital.ecommerce.customer.entity.Customer;
 
 public class CustomerDao extends Dao {
-	
-	
+
 	public boolean selectCustomerByEmailAndPassword(Customer customer) {
 		boolean status = false;
-		
+
 		Connection con = openConnection();
 		Statement stmt = openStatement(con);
 		ResultSet rs = null;
-		
+
 		try {
-			String sql = "SELECT * FROM customer_info WHERE email = '" + customer.getEmail() + "' AND password = '" + customer.getPassword() + "'";
-			
+			String sql = "SELECT * FROM customer_info WHERE email = '" + customer.getEmail() + "' AND password = '"
+					+ customer.getPassword() + "'";
+
 			rs = stmt.executeQuery(sql);
-			if(rs.next()) {
+			if (rs.next()) {
 				status = true;
-				
+
 				customer.setId(rs.getInt(1));
 				customer.setFirstName(rs.getString(2));
 				customer.setLastName(rs.getString(3));
-				
+
 			}
 		} catch (SQLException sqle) {
 			sqle.printStackTrace();
@@ -39,24 +39,23 @@ public class CustomerDao extends Dao {
 			closeResultSet(rs);
 			closeConnection(con);
 		}
-		
+
 		return status;
 	}
-	
+
 	public boolean insertCustomer(Customer customer) {
 		boolean status = false;
-		
-		
+
 		Connection con = openConnection();
 		Statement stmt = openStatement(con);
-		
+
 		try {
-			String sql = "INSERT INTO customer_info(fname, lname,email,password) " +
-						 "VALUES('" + customer.getFirstName() + "', '" + customer.getLastName() +
-						 "', '" + customer.getEmail() + "', '" + customer.getPassword() + "')";
-			
+			String sql = "INSERT INTO customer_info(fname, lname,email,password) " + "VALUES('"
+					+ customer.getFirstName() + "', '" + customer.getLastName() + "', '" + customer.getEmail() + "', '"
+					+ customer.getPassword() + "')";
+
 			int rows = stmt.executeUpdate(sql);
-			if(rows != 0) {
+			if (rows != 0) {
 				status = true;
 			}
 		} catch (SQLException sqle) {
@@ -65,7 +64,7 @@ public class CustomerDao extends Dao {
 			closeStatement(stmt);
 			closeConnection(con);
 		}
-		
+
 		return status;
 	}
 
@@ -73,10 +72,10 @@ public class CustomerDao extends Dao {
 		Connection con = openConnection();
 		Statement stmt = openStatement(con);
 		try {
-			String sql = "UPDATE customer_info SET password='"+newpwd+"'WHERE email='"+customer.getEmail()+"'";
-			
+			String sql = "UPDATE customer_info SET password='" + newpwd + "'WHERE email='" + customer.getEmail() + "'";
+
 			int rows = stmt.executeUpdate(sql);
-			if(rows != 0) {
+			if (rows != 0) {
 				return true;
 			}
 		} catch (SQLException sqle) {
@@ -85,10 +84,32 @@ public class CustomerDao extends Dao {
 			closeStatement(stmt);
 			closeConnection(con);
 		}
-		
+
 		return false;
 	}
-	
-	
-	
+
+	public boolean registerPersonalDetails(Customer customer) {
+		Connection con = openConnection();
+		Statement stmt = openStatement(con);
+		try {
+			String sql = "UPDATE customer_info SET  gender='" + customer.getGender() + "',address='"
+					+ customer.getAddress() + "',city='" + customer.getCity() + "',zip='" + customer.getZip() + "',state='"
+					+ customer.getState() + "',country='" + customer.getCountry() + "',contact='" + customer.getContact() + "'WHERE email='"
+					+ customer.getEmail() + "'";
+
+			int rows = stmt.executeUpdate(sql);
+			if (rows != 0) {
+				return true;
+			}
+		} catch (SQLException sqle) {
+			sqle.printStackTrace();
+		} finally {
+			closeStatement(stmt);
+			closeConnection(con);
+		}
+
+		return false;
+
+	}
+
 }
